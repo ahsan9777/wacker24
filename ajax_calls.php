fb8c47cc6787ac7a65de63ccd50065e4c89d30c5
Ahsan Nawaz Khan <ahsannawaz9777"gmail.com>
2025-09-22 00:19:19 +0500

    product category page alt and title attributes

--- ajax_calls.php
+++ ajax_calls.php
@@ -717,9 +717,9 @@ if (isset($_REQUEST['action'])) {
                         $cat_link = "artikelarten/" . $row->cat_params . "/" . $row->sub_cat_params;
                     }
                     $category_type_inner .= '<div class="ctg_type_col">
-												<a href="' . $cat_link . '">
+												<a href="' . $cat_link . '" title = "' . $row->sub_cat_title . '">
 													<div class="ctg_type_card">
-														<div class="ctg_type_image"><img loading="lazy" src="' . get_image_link(160, $pg_mime_source_url_href) . '" alt=""></div>
+														<div class="ctg_type_image"><img loading="lazy" src="' . get_image_link(160, $pg_mime_source_url_href) . '" alt="' . $row->sub_cat_title . '"></div>
 														<div class="ctg_type_detail">
 															<div class="ctg_type_title">' . $row->sub_cat_title . '</div>
 															<div class="ctg_type_price price_without_tex" ' . $price_without_tex_display . ' > ab ' . price_format(($pbp_price_without_tax > 0) ? $pbp_price_without_tax : 0.00) . ' €</div>
